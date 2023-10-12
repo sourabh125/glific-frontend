415936dc999bf5f2f66cdf2c7d961a642d0ad15d
mdshamoon <mohdshamoon841@gmail.com>
2023-10-12 10:18:34 +0530

    allow name to be changed

--- src/containers/Profile/Profile.tsx
+++ src/containers/Profile/Profile.tsx
@@ -134,7 +134,6 @@ export const Profile = ({
       component: Input,
       name: 'name',
       type: 'text',
-      disabled: profileType === 'Contact',
       placeholder: t('Name'),
     },
     {
